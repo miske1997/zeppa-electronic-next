d81f4e7df662f628f429ad269199c8c6f59ac65a
miske <miskestoiljkovic@gmail.com>
2024-09-18 12:31:01 +0200

    pictures added to mobile carousel

--- src/components/MobileHomeCarousel/MobileHomeCarousel.js
+++ src/components/MobileHomeCarousel/MobileHomeCarousel.js
@@ -7,18 +7,21 @@ function MobileHomeCarousel() {
         <div className="mobile-carousel">
             <Carousel indicators={false} >
                 <Carousel.Item>
+                    <img src="/arduino-uno.png"></img>
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
+                        <h3>Najnoviji Arduino</h3>
+                        <p>U nasem katalogu sada najnoviji arduino MAKRO</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/sensor.png"></img>
+                    <Carousel.Caption>
+                        <h3>Senzori</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
+                    <img src="/bord.png"></img>
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
