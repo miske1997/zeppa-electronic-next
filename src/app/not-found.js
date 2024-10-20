d20c1fdfad93b30ec642542f3f0f3590d0a1ca39
milos stoiljkovic <miskestoiljkovic@gmail.com>
2024-10-20 15:11:03 +0200

    not found page added

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+
+
+function NotFound() {
+    return (
+        <div style={{height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <h1>Stranica ne postoji</h1>
+        </div> 
+     );
+}
+
+export default NotFound;
